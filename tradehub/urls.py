14118e0ae574f19ae43df128f798e7ae553e82b9
Sandeepbhiwani <sandeepbhiwani403@gmail.com>
2025-11-29 18:01:10 +0530

    Add migration endpoints for running migrations

--- tradehub/urls.py
+++ tradehub/urls.py
@@ -4,15 +4,29 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
+# Import migration helpers from home.views
+from home.views import run_migrations
+from home.views import run_fake   # <-- added fake migration runner
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Home pages
     path('', include('home.urls')),
+
+    # Apps
     path('accounts/', include('accounts.urls')),
     path('payments/', include('payments.urls')),
     path('assets/', include('assets.urls')),
     path('stock/', include('stockmanagement.urls')),
     path('dashboard/', include('dashboard.urls')),
+
+    # Migration endpoints
+    path("run-migrations/", run_migrations),  # full migration
+    path("run-fake/", run_fake),              # fake apply migration
 ]
+
+# STATIC & MEDIA
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
